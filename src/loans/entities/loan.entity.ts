44e18a3b4840b15777b0c9a0653bc0d00f77437e
JulianVR09 <julianvillacis.02@gmail.com>
2024-08-14 20:14:23 -0500

    First commit

---
+++ src/loans/entities/loan.entity.ts
@@ -0,0 +1,20 @@
+import { User } from "src/users/entities/user.entity"
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Loan{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    amount: number;
+
+    @Column({default: 2})
+    interesTate: number;
+
+    @Column({default: true})
+    status: boolean;
+
+    @ManyToOne(() => User, (user) => user.loans)
+    user: User;
+}
